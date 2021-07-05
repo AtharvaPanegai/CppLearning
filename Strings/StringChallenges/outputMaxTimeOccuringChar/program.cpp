be678f8def48e874a838ad68e3e1d683c0671893
AtharvaPanegai <panegai.atharv@gmail.com>
2021-07-05 18:57:10 +0530

    MaxOccuringNumber

---
+++ Strings/StringChallenges/outputMaxTimeOccuringChar/program.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main(){
+
+    string s = "bhsvdkskrfkvbskjdvb";
+    int freq[26];
+    for (int i = 0; i < 26; i++)
+    {
+            freq[i] = 0;
+    }
+    for (int i = 0; i < s.size(); i++)
+    {
+        freq[s[i]-'a']++;
+    }
+    int maxFreq = -1;
+    int maxChar = 'a';
+
+    for (int i = 0; i < 26; i++)
+    {
+        if(freq[i]>maxFreq){
+            maxFreq = freq[i];
+            maxChar = i+'a';
+        }
+    }
+    cout<<maxFreq<<" "<<maxChar;
+    return 0;
+}
