60b8ae1040c857784dd88ee8e115639a991debbc
AtharvaPanegai <panegai.atharv@gmail.com>
2021-07-09 15:55:51 +0530

    RecursionSecond

--- Recursion/CalculateNRaisedToP/program.cpp
+++ Recursion/CalculateNRaisedToP/program.cpp
@@ -1,7 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int raised(int n, int p)
+{
+    if (p == 0)
+    {
+        return 1;
+    }
+    int prevPower = raised(n, p - 1);
+    return n * prevPower;
+}
+
+int main()
+{
+
+    int n, p;
+    cin >> n >> p;
+    cout << raised(n, p);
 
     return 0;
 }
