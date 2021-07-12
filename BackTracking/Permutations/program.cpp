b4f77b7b52ddf22dc65f484d6ce52b409ead5396
AtharvaPanegai <panegai.atharv@gmail.com>
2021-07-12 19:20:21 +0530

    nQueen

---
+++ BackTracking/Permutations/program.cpp
@@ -0,0 +1,11 @@
+// given an array nums of ditinct integers. return all the possible permutations you can return the answer in any order
+
+// Permutation Problem After Vdo no 48
+#include "bits/stdc++.h"
+using namespace std;
+
+
+int main(){
+
+    return 0;
+}
