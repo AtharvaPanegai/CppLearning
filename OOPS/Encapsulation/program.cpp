3ffaa16e2ad6c95126fc84fe271ee9f0f2778fe4
AtharvaPanegai <panegai.atharv@gmail.com>
2021-07-23 15:33:46 +0530

    Encapsulation

---
+++ OOPS/Encapsulation/program.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class A
+{
+public:
+    int a;
+    void funcA()
+    {
+        cout << "Function A\n";
+    }
+
+private:
+    int b;
+    void funcB()
+    {
+        cout << "Function B\n";
+    }
+
+protected:
+    int c;
+    void funcC()
+    {
+        cout << "Function C\n";
+    }
+};
+
+int main()
+{
+    A obj;
+    obj.funcA();
+    return 0;
+}
