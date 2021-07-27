91f24f2044c585610d30c1f450274635cab94901
AtharvaPanegai <panegai.atharv@gmail.com>
2021-07-27 16:20:32 +0530

    Minor Changes

--- DSA/Array/ArrayChallenges/subArraySequene/sumOfSubArrays/program.cpp
+++ DSA/Array/ArrayChallenges/subArraySequene/sumOfSubArrays/program.cpp
@@ -1,28 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
     int sum = 0;
     for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         sum = 0;
-        for (int j = i; j < n-1; j++)
+        for (int j = i; j < n - 1; j++)
         {
-            sum +=arr[j];
-            cout<<sum<<" "<<endl;;
+            sum += arr[j];
+            cout << sum << " " << endl;
         }
-        
     }
-    
-    
 
     return 0;
 }
