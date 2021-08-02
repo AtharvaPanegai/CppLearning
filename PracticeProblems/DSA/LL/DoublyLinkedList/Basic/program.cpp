fed8c6cd5a43fd995ddfc51febcc2026033a1970
AtharvaPanegai <panegai.atharv@gmail.com>
2021-08-02 18:11:54 +0530

    BasicOperationOfDoublyLinkedList

--- PracticeProblems/DSA/LL/DoublyLinkedList/Basic/program.cpp
+++ PracticeProblems/DSA/LL/DoublyLinkedList/Basic/program.cpp
@@ -29,7 +29,7 @@ void insertAtHead(node *&head, int val)
     head = n;
 }
 
-void insertAtTail(node *&head,int val)
+void insertAtTail(node *&head, int val)
 {
 
     if (head == NULL)
@@ -54,22 +54,62 @@ void insertAtTail(node *&head,int val)
 void display(node *&head)
 {
     node *temp = head;
-    while(temp!=NULL){
-        cout<<temp->data<<"->";
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
         temp = temp->next;
-
     }
-    cout<<"NULL";
+    cout << "NULL";
+}
+
+void deleteAtHead(node *&head){
+    
+    node *toDelete = head;
+    head = head->next;
+    head->prev = NULL;
+
+    delete toDelete;
+}
+
+void deletion(node *&head, int pos)
+{
+    if(pos==1){
+        deleteAtHead(head);
+    }
+
+    node *temp = head;
+    int count = 1;
+
+    while (temp->next != NULL)
+    {
+        if (count == pos)
+        {
+            temp->prev->next = temp->next;
+            if (temp->next != NULL)
+            {
+                temp->next->prev = temp->prev;
+            }
+
+            node *toDelete = temp;
+            delete temp;
+
+            return;
+        }
+        count++;
+        temp = temp->next;
+    }
 }
 
 int main()
 {
     node *head = NULL;
-    insertAtTail(head,1);
-    insertAtTail(head,2);
-    insertAtTail(head,3);
-    insertAtTail(head,4);
-    insertAtTail(head,5);
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+
+    deletion(head, 5);
 
     display(head);
 
