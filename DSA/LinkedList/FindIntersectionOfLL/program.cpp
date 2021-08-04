f154bac0115629f796b4f3f7aa6c8c6c04f458da
AtharvaPanegai <panegai.atharv@gmail.com>
2021-08-04 23:28:54 +0530

    Merge 2 SortedArrays

--- DSA/LinkedList/FindIntersectionOfLL/program.cpp
+++ DSA/LinkedList/FindIntersectionOfLL/program.cpp
@@ -1,8 +1,113 @@
+// ReWatch
+
 #include "bits/stdc++.h"
 using namespace std;
 
-int main(){
+class node
+{
+public:
+    int data;
+    node *next;
 
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *&head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+int lengthOfLL(node *&head)
+{
+    node *temp = head;
+    int l = 0;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+        l++;
+    }
+
+    return l;
+}
+
+int interSection(node *&head1, node *&head2)
+{
+    int l1 = lengthOfLL(head1);
+    int l2 = lengthOfLL(head2);
+
+    int d = 0;
+    node *ptr1; // bigger
+    node *ptr2; // smaller
+
+    // check which LL is bigger 
+
+    if(l1>l2){
+        d = l1-l2;
+        ptr1 = head1;
+        ptr2 = head2;
+    }
+
+    else{
+        d =l2-l1;
+        ptr1 = head2;
+        ptr2 = head1;
+    }
+
+    while(d){
+        ptr1 = ptr1->next;
+        if(ptr1==NULL){
+            return -1;
+        }
+        d--;
+    }
+
+
+    while(ptr1!=NULL && ptr2==NULL){
+        if(ptr1==ptr2){
+            return ptr1->data;
+        }
+
+    }
+}
+
+int main()
+{
+    node *head = NULL;
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    for (int i = 0; i < 7; i++)
+    {
+        insertAtTail(head, arr[i]);
+    }
+
+    display(head);
 
     return 0;
 }
---
+++ DSA/LinkedList/Merge2SortedLL/program.cpp
@@ -0,0 +1,107 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *&head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+node *merge(node *&head1, node *&head2)
+{
+    node *p1 = head1;
+    node *p2 = head2;
+
+    node *dummyNode = new node(-1);
+    node *p3=dummyNode;
+
+    while(p1!=NULL && p2!=NULL){
+        if(p1->data<p2->data){
+            p3->next = p1;
+            p1=p1->next;
+        }
+        else{
+            p3->next = p2;
+            p2 = p2->next;
+        }
+        p3 = p3->next;
+    }
+
+    while(p1!=NULL){
+        p3->next = p1;
+        p1 = p1->next;
+        p3 = p3->next;
+    }
+
+    while(p2!=NULL){
+        p3->next = p2;
+        p1 = p1->next;
+        p3 = p3->next;
+    }
+
+    return dummyNode->next;
+}
+
+int main()
+{
+    node *headOne = NULL;
+    node *headTwo = NULL;
+
+    int arrOne[] = {1,4,5,7};
+    int arrTwo[] = {2,6,8};
+
+    for (int i = 0; i < 4; i++)
+    {
+        insertAtTail(headOne,arrOne[i]);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        insertAtTail(headTwo,arrTwo[i]);
+    }
+
+    display(headOne);
+    display(headTwo);
+
+    node *mergedLLHead = merge(headOne,headTwo);
+
+    display(mergedLLHead);
+    
+
+    return 0;
+}
