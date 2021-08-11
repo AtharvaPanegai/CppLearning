f18c5a011406d2f2dba27ae100de11ea138a90cc
AtharvaPanegai <panegai.atharv@gmail.com>
2021-08-11 23:39:42 +0530

    Reverse a Stack

--- DSA/Stack/ReverseAStack/program.cpp
+++ DSA/Stack/ReverseAStack/program.cpp
@@ -1,8 +1,49 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+void insertAtBottom(stack<int> &st,int ele){
+
+    if(st.empty()){
+        st.push(ele);
+        return;
+    }
+
+
+    int topEle = st.top();
+    st.pop();
+    insertAtBottom(st,ele);
+
+    st.push(topEle);
+}
+
+void reverseFunction(stack<int> &st){
+
+    if(st.empty()){
+        return;
+    }
+
+    int ele = st.top();
+    st.pop();
+    reverseFunction(st);
+    insertAtBottom(st,ele);
+}
+
 int main()
 {
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+
+    reverseFunction(st);
+
+    while(!st.empty()){
+        cout<<st.top()<<endl;
+        st.pop();
+    }cout<<"\n";
 
     return 0;
 }
