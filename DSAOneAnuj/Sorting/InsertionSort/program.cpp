f6c0c116f72146c801ee82ec34f7ea85e9540d2e
AtharvaPanegai <panegai.atharv@gmail.com>
2022-01-22 13:31:07 +0530

    Insertion Sort

---
+++ DSAOneAnuj/Sorting/InsertionSort/program.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
+int main()
+{
+    int arr[] = {14, 145, 4, 7, 1, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
