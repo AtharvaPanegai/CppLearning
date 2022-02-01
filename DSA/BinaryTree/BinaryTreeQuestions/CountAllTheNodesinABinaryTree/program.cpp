4ed1e4294ab7487f2a9816e90d1d1be4e5dfbb00
AtharvaPanegai <panegai.atharv@gmail.com>
2022-02-01 12:15:14 +0530

    SumOfNodes and Number of Nodes in Binary Tree

--- DSA/BinaryTree/BinaryTreeQuestions/CountAllTheNodesinABinaryTree/program.cpp
+++ DSA/BinaryTree/BinaryTreeQuestions/CountAllTheNodesinABinaryTree/program.cpp
@@ -1,12 +1,11 @@
 #include "bits/stdc++.h"
 using namespace std;
-int numberOfNodes = 0;
 
 struct Node
 {
     int data;
-    struct Node *left;
-    struct Node *right;
+    Node *left;
+    Node *right;
 
     // constructor
     Node(int val)
@@ -17,28 +16,36 @@ struct Node
     }
 };
 
-int sumofNodes(Node *root)
+int numberOfNodes(Node *root)
 {
-
     if (root == NULL)
     {
         return 0;
     }
 
-    return sumofNodes(root->left) + sumofNodes(root->right) + 1;
+    return numberOfNodes(root->left) + numberOfNodes(root->right) + 1;
+}
+
+int sumOfNodes(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    return sumOfNodes(root->left) + sumOfNodes(root->right) + root->data;
 }
 
 int main()
 {
     struct Node *root = new Node(1);
     root->left = new Node(2);
-    root->left->left = new Node(4);
-    root->left->right = new Node(2);
     root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
     root->right->left = new Node(6);
     root->right->right = new Node(7);
 
-    cout << sumofNodes(root);
+    cout << sumOfNodes(root);
 
     return 0;
 }
